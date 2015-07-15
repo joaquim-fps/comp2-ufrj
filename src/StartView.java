2964e17e3c0ef7d5196d7952f753acdcc62101dc
b-Ferraz- <brunoferraz24@gmail.com>
2015-07-14 23:33:00 -0300

    #Modificando StartView
    Complementando a classe StartView para completar a mesma
    - Nome do Jogo
    - Nome dos Integrantes
    - Projetando botões das demais telas

--- src/StartView.java
+++ src/StartView.java
@@ -1,28 +1,40 @@
-import java.awt.Color;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+
+import javax.swing.*;
 
 public class StartView
 {
 	private JFrame frame;
 	private JPanel panel;
-	private JButton button;
+	private JButton bPlay, bInstr , bConfig ;
 	private Controller controller;
+	private JLabel gamesName,group;
+	private String player;
 	
 	public StartView()
 	{
-		button = new JButton("Play");
-		button.addActionListener(new ButtonListener());
+		gamesName = new JLabel(" PokeBalls Drifting ");
+		group = new JLabel("Bruno Ferraz \n Joaquim Ferreira \n Vitor Barcellos");
+		Font font = new Font("serif",Font.BOLD,32);
+		gamesName.setFont(font);
+		
+		
+		bPlay = new JButton("Play");
+		bPlay.addActionListener(new ButtonListener());
+		bPlay.setBackground(Color.LIGHT_GRAY);
 		
 		panel = new JPanel();
 		panel.setBackground(Color.white);
-		panel.add(button);
+		BoxLayout box = new BoxLayout( panel , BoxLayout.Y_AXIS );
+		panel.setLayout(box);
+		panel.add(gamesName);
+		panel.add(group);
 		
 		frame = new JFrame("Drifts");
-		frame.getContentPane().add(panel);
+		frame.getContentPane().add(BorderLayout.NORTH,panel);
+		frame.getContentPane().add(bPlay);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(300, 300);
 		frame.setBounds(0, 0, 300, 300);
@@ -51,11 +63,11 @@ public class StartView
 	}
 
 	public JButton getButton() {
-		return button;
+		return bPlay;
 	}
 
-	public void setButton(JButton button) {
-		this.button = button;
+	public void setButton(JButton bPlay) {
+		this.bPlay = bPlay;
 	}
 
 	public Controller getController() {
@@ -64,6 +76,10 @@ public class StartView
 
 	public void setController(Controller controller) {
 		this.controller = controller;
+	}
+
+	public String getPlayer() {
+		return player;
 	}
 
 	class ButtonListener implements ActionListener
@@ -76,7 +92,15 @@ public class StartView
 
 		        @Override
 		        public void run() {
-		        	controller.startGame();
+		        	Object o = event.getSource();
+		        	if(o.equals(bPlay))
+		        	{
+		        		controller.startGame();
+		        	}
+		        	/*else if(o.equals())
+		        	{
+		        	
+		        	}*/
 
 		        }
 		    }.start();
