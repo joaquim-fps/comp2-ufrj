717f2d8471e8f676af90598fd5ec68a981499e4c
vband <vtor.barcellos7@gmail.com>
2015-07-16 16:45:37 -0300

    Bolas nascem mais uniformemente
    Antes, quando as bolas nasciam, elas sempre começavam se movendo para a
    direita. Agora, o lado para que elas se movem inicialmente é decidido
    aleatoriamente.

--- src/CherishBall.java
+++ src/CherishBall.java
@@ -4,15 +4,28 @@ import java.awt.Color;
 public class CherishBall extends PokeBall
 {
 	public CherishBall(World world, double radius, Color color) {
-		super(radius, color);
+super(radius, color);
 		
+		int sinal1, sinal2;
 		double x = world.getWidth() * Math.random();
 		
 		x = (x + radius >= world.getWidth()) ? x - (radius+1) : x;
 		x = (x - radius <= 0) ? x + (radius+1) : x;
 		
+		if ((int)(Math.random()*100) % 2 == 0)
+		{
+			sinal1 = 1;
+		}
+		else sinal1 = -1;
+		
+		if ((int)(Math.random()*100) % 2 == 0)
+		{
+			sinal2 = 1;
+		}
+		else sinal2 = -1;
+		
 		super.setCenter(new Point(x, radius+1));
-		super.setV(new Velocity(3 * Math.random(), -3 * Math.random()));
+		super.setV(new Velocity(sinal1 * 3 * Math.random(), sinal2 * 3 * Math.random()));
 	}
 	
 	public boolean hasCollidedWithBottom(World world)
